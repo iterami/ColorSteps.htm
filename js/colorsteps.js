885652657aaa74fb0e4113980198e4759348bd9f
honzi <czechinthemail@gmail.com>
2016-05-01 14:54:10 -0700

    switched to usage of common canvas.js

--- js/colorsteps.js
+++ js/colorsteps.js
@@ -1,5 +1,72 @@
 'use strict';
 
+function draw_logic(){
+    var color_left = random_hex();
+    var color_right = random_hex();
+    document.getElementById('canvas').style.background = random_hex();
+
+    var row = Math.floor(height / 80);
+    do{
+        var row_y = height - row * 80 - 50;
+
+        buffer.fillStyle = color_left;
+        var column = Math.floor(width / 200);
+        do{
+            var column_x =
+              column * 200
+              + (row % 2 === 0 ? 100 : 0);
+
+            buffer.beginPath();
+            buffer.moveTo(
+              column_x,
+              row_y
+            );
+            buffer.lineTo(
+              column_x + 100,
+              row_y - 30
+            );
+            buffer.lineTo(
+              column_x + 100,
+              row_y + 20
+            );
+            buffer.lineTo(
+              column_x,
+              row_y + 50
+            );
+            buffer.closePath();
+            buffer.fill();
+        }while(column--);
+
+        buffer.fillStyle = color_right;
+        column = Math.floor(width / 200);
+        do{
+            column_x =
+              column * 200
+              - (row % 2 === 0 ? 100 : 0);
+
+            buffer.beginPath();
+            buffer.moveTo(
+              column_x + 100,
+              row_y - 30
+            );
+            buffer.lineTo(
+              column_x + 200,
+              row_y
+            );
+            buffer.lineTo(
+              column_x + 200,
+              row_y + 50
+            );
+            buffer.lineTo(
+              column_x + 100,
+              row_y + 20
+            );
+            buffer.closePath();
+            buffer.fill();
+        }while(column--);
+    }while(row--);
+}
+
 function random_hex(){
     var choices = '0123456789abcdef';
     return '#'
@@ -8,89 +75,11 @@ function random_hex(){
       + choices.charAt(Math.floor(Math.random() * 16));
 }
 
-function resize(){
-    var color_left = random_hex();
-    var color_right = random_hex();
-    var height = window.innerHeight;
-    var width = window.innerWidth;
-
-    document.getElementById('canvas').height = height;
-    document.getElementById('canvas').style.background = random_hex();
-    document.getElementById('canvas').width = width;
-
-    canvas.clearRect(
-      0,
-      0,
-      width,
-      height
-    );
-
-    var row = Math.floor(height / 80);
-    do{
-        var row_y = height - row * 80 - 50;
-
-        canvas.fillStyle = color_left;
-        var column = Math.floor(width / 200);
-        do{
-            var column_x =
-              column * 200
-              + (row % 2 === 0 ? 100 : 0);
-
-            canvas.beginPath();
-            canvas.moveTo(
-              column_x,
-              row_y
-            );
-            canvas.lineTo(
-              column_x + 100,
-              row_y - 30
-            );
-            canvas.lineTo(
-              column_x + 100,
-              row_y + 20
-            );
-            canvas.lineTo(
-              column_x,
-              row_y + 50
-            );
-            canvas.closePath();
-            canvas.fill();
-        }while(column--);
-
-        canvas.fillStyle = color_right;
-        column = Math.floor(width / 200);
-        do{
-            column_x =
-              column * 200
-              - (row % 2 === 0 ? 100 : 0);
-
-            canvas.beginPath();
-            canvas.moveTo(
-              column_x + 100,
-              row_y - 30
-            );
-            canvas.lineTo(
-              column_x + 200,
-              row_y
-            );
-            canvas.lineTo(
-              column_x + 200,
-              row_y + 50
-            );
-            canvas.lineTo(
-              column_x + 100,
-              row_y + 20
-            );
-            canvas.closePath();
-            canvas.fill();
-        }while(column--);
-    }while(row--);
+function resize_logic(){
+    draw();
 }
 
-var canvas = document.getElementById('canvas').getContext('2d');
-
-window.onkeyup = resize;
-window.onload = resize;
-window.onmousedown = resize;
-window.onresize = resize;
-window.ontouchstart = resize;
+window.onkeyup = draw_logic;
+window.onload = init_canvas;
+window.onmousedown = draw_logic;
+window.ontouchstart = draw_logic;
