46bab7e8c479bebe9c742ca8d325cb1ff1b7933a
Jan Hořava <czechinthemail@gmail.com>
2024-01-10 04:46:21 -0800

    updated core_storage_save() usage

--- repo.js
+++ repo.js
@@ -110,11 +110,9 @@ function update_colors(){
     color_right = '#' + core_random_hex();
     canvas_properties['clearColor'] = '#' + core_random_hex();
 
-    core_storage_save({
-      'keys': [
-        'step-x',
-        'step-y',
-      ],
-    });
+    core_storage_save([
+      'step-x',
+      'step-y',
+    ]);
     canvas_draw();
 }
